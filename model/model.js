da8b64a1c6b0ea483002a2537511790a3796d328
Kelum Sampath Edirisinghe <164193E@uom.lk>
2018-10-15 19:16:59 +0530

    Update model.js

--- model/model.js
+++ model/model.js
@@ -2,17 +2,14 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-mongoose.connect("mongodb://localhost:27017/chat");
+var dbURI = "mongodb://localhost:27017/chat";
+mongoose.connect(dbURI, { useNewUrlParser: true });
 
-mongoose.connection.on('open', function (ref) {
-    console.log('Connected to mongo server.');
-});
-mongoose.connection.on('error', function (err) {
-    console.log('Could not connect to mongo server!');
-    console.log(err);
+// Throw an error if the connection fails
+mongoose.connection.on('error', function(err) {
+	if(err) throw err;
 });
 
-mongoose.connect('mongodb://localhost/mongodb');
 
 module.exports.user=mongoose.model('User',new Schema({
     name:String,
